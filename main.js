dbb4d2a480ae346ea11573346c8450180e91d042
edward salcido <edwardromero51609@gmail.com>
2021-04-20 07:57:09 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(){
+    //Your code goes here
+}
